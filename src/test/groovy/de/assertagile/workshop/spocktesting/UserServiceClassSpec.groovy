77e0e4ebc04938496963f1bca8586ff2455d96f8
Michael Kutz <michael.kutz@rewe-digital.com>
2017-04-15 12:29:48 +0200

    Solved objective 3.1.

---
+++ src/test/groovy/de/assertagile/workshop/spocktesting/UserServiceClassSpec.groovy
@@ -0,0 +1,37 @@
+package de.assertagile.workshop.spocktesting
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+import java.time.LocalDate
+
+class UserServiceClassSpec extends Specification {
+
+    UserRepository userRepositoryMock = Mock()
+
+    @Subject
+    UserService userService = new UserService(userRepositoryMock)
+
+    def "registerUser persists the user data via UserRepository (real User)"() {
+        given:
+        User user = new User("jdoe", "John", "Doe", LocalDate.now().minusYears(18))
+
+        when:
+        userService.registerUser(user)
+
+        then:
+        1 * userRepositoryMock.saveUser(user.toEntity())
+    }
+
+    def "registerUser persists the user data via UserRepository (Mock)"() {
+        given:
+        UserEntity userEntityMock = Mock(UserEntity)
+        User userMock = Mock() { toEntity() >> userEntityMock }
+
+        when:
+        userService.registerUser(userMock)
+
+        then:
+        1 * userRepositoryMock.saveUser(userEntityMock)
+    }
+}
